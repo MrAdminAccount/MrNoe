517ee1bc45721cb05bf688b17bd904112680e437
MrAdminAccount <86154676+MrAdminAccount@users.noreply.github.com>
2022-10-04 23:40:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("Database Connected")).catch(() => console.log("Database Connection Failed"))
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+  allowedMentions: {
+    parse: ['users', 'roles'],
+    repliedUser: false
+  },
+});
 
 client.commands = new Collection();
 client.timeouts = new Collection();
